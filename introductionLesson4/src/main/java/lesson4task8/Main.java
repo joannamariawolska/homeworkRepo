79cd9aeff02f913c56fcfc73eaab0a8c48a580be
Joanna Wolska <joanna.maria.wolska@gmail.com>
2023-01-03 15:53:21 +0100

    not finished

---
+++ introductionLesson4/src/main/java/lesson4task8/Main.java
@@ -0,0 +1,16 @@
+package lesson4task8;
+
+public class Main {
+     public static void main(String[] args) {
+
+         Person person1 = new Person("Anna", "Nowak", 30);
+         Person person2 = new Person("Maria", "Kowalska", 35);
+         Person person3 = new Person("Maria", "Kowalska", 35);
+
+         System.out.println(person1.equals(person2));
+         System.out.println(person2.equals(person3));
+         System.out.println(person1.hashCode());
+
+
+    }
+}
---
+++ introductionLesson4/src/main/java/lesson4task8/Person.java
@@ -0,0 +1,29 @@
+package lesson4task8;
+
+public class Person {
+
+    String name;
+    String surname;
+    int age;
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getName() + "@" + Integer.toHexString(hashCode());
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
