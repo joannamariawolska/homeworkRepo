38652a9ffcec3c51c8ac24728681fd0745d1cc3c
Joanna Wolska <joanna.maria.wolska@gmail.com>
2023-01-12 09:10:03 +0100

    renamed class name

--- introductionLesson6/src/main/java/lesson6task6/TXTSaver.java
+++ introductionLesson6/src/main/java/lesson6task6/TXTSaver.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class  TXTSave{
+public class  TXTSaver{
 
     public static void writeToFile(String pathToFile, String upperCaseText) {
         try {
