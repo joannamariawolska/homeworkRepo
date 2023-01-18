f2898a757b81062396980445670994dbfcfb2ada
Joanna Wolska <joanna.maria.wolska@gmail.com>
2023-01-18 13:21:29 +0100

    Text class and TextTest

---
+++ introductionLesson7/src/main/java/lesson7task4/Text.java
@@ -0,0 +1,7 @@
+package lesson7task4;
+
+public class Text {
+    public static int getLength(String string){
+        return string.length();
+    }
+}
---
+++ introductionLesson7/src/test/java/lesson7task4/TextTest.java
@@ -0,0 +1,26 @@
+package lesson7task4;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class TextTest {
+
+    @ParameterizedTest
+    @MethodSource("getLength")
+    void shouldGetStringLength(String input, int expected) {
+        int actualValue = input.length();
+        Assertions.assertEquals(expected, actualValue);
+    }
+
+    public static Stream<Arguments> getLength(){
+        return Stream.of(
+                Arguments.of("Samochód", 8),
+                Arguments.of("Auto", 4),
+                Arguments.of("Koszyk", 6)
+        );
+    }
+}
