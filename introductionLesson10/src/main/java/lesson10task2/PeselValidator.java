5c0bef2f6f9bdda08ba2506b22a39319e8f31303
Joanna Wolska <joanna.maria.wolska@gmail.com>
2023-01-28 18:23:43 +0100

    renamed matcher

--- introductionLesson10/src/main/java/lesson10task2/PeselValidator.java
+++ introductionLesson10/src/main/java/lesson10task2/PeselValidator.java
@@ -7,7 +7,7 @@ public class PeselValidator {
    public static boolean checkPesel(String pesel) {
 
       Pattern pattern = Pattern.compile("\\d{11}");
-      Matcher matcher1 = pattern.matcher(pesel);
-      return matcher1.matches();
+      Matcher matcher = pattern.matcher(pesel);
+      return matcher.matches();
    }
 }
