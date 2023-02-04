636e39da1108cc477cf7119d07e673d0d204e37d
Joanna Wolska <joanna.maria.wolska@gmail.com>
2023-02-04 11:55:45 +0100

    corrected

--- introductionLesson11/src/main/java/lesson11task5/UniqueLetters.java
+++ introductionLesson11/src/main/java/lesson11task5/UniqueLetters.java
@@ -1,23 +1,19 @@
 package lesson11task5;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class UniqueLetters {
-    public static Set<Character> getLetters(){
 
-        List<String> words = Arrays.asList("FutureCollars", "java", "homework", "HelloWorld");
+    public static Set<Character> getUniqueLetters(List<String> input){
 
-        Set<Character> uniqueLetters = words.stream()
+        return input.stream()
                 .map(word -> word.toUpperCase())
                 .flatMap(word -> word.chars().mapToObj(c -> (char) c))
                 .collect(Collectors.toSet());
-        return uniqueLetters;
     }
 
     public static void main(String[] args) {
-        System.out.println(UniqueLetters.getLetters());
+        System.out.println(getUniqueLetters(List.of("java", "FutureCollars", "OOP", "programowanie")));
     }
 }
