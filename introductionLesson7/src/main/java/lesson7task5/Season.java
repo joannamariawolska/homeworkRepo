ac14069b3db27ad93925aac51c2b23d674a70008
Joanna Wolska <joanna.maria.wolska@gmail.com>
2023-02-04 14:40:44 +0100

    corrected

--- introductionLesson7/src/main/java/lesson7task5/Season.java
+++ introductionLesson7/src/main/java/lesson7task5/Season.java
@@ -1,7 +1,5 @@
 package lesson7task5;
 
-import java.time.Month;
-
 public enum Season {
     WINTER,
     SPRING,
@@ -9,30 +7,3 @@ public enum Season {
     AUTUMN
 }
 
-class SeasonUtils {
-
-    public static String printSeason(Month month) {
-        if(month == null) {
-            throw new IllegalArgumentException("Month cannot be null");
-        }else switch (month) {
-            case DECEMBER:
-            case JANUARY:
-            case FEBRUARY:
-                return Season.WINTER.name();
-            case MARCH:
-            case APRIL:
-            case MAY:
-                return Season.SPRING.name();
-            case JUNE:
-            case JULY:
-            case AUGUST:
-                return Season.SUMMER.name();
-            case SEPTEMBER:
-            case OCTOBER:
-            case NOVEMBER:
-                return Season.AUTUMN.name();
-            default:
-                return "There is no such a month";
-        }
-    }
-}
--- introductionLesson7/src/test/java/lesson7task5/SeasonUtilsTest.java
+++ introductionLesson7/src/test/java/lesson7task5/SeasonUtilsTest.java
@@ -4,39 +4,41 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+
 import java.time.Month;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SeasonUtilsTest {
 
     @Test
-    void shouldThrowIllegalArgumentException() {
-        assertThrows(IllegalArgumentException.class, () ->  {
+    void shouldThrowIllegalArgumentExceptionWithCorrectMessage() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->  {
             SeasonUtils.printSeason(null);
         });
+        Assertions.assertEquals("Month cannot be null", exception.getMessage());
     }
 
     @ParameterizedTest
     @EnumSource(value = Month.class, names = {"DECEMBER", "JANUARY", "FEBRUARY"})
     void shouldIndicateWinter(Month month) {
-        Assertions.assertEquals(Season.WINTER.toString(), SeasonUtils.printSeason(month));
+        Assertions.assertEquals(Season.WINTER, SeasonUtils.printSeason(month));
     }
 
     @ParameterizedTest
     @EnumSource(value = Month.class, names = {"MARCH", "APRIL", "MAY"})
     void shouldIndicateSpring(Month month) {
-        Assertions.assertEquals(Season.SPRING.toString(), SeasonUtils.printSeason(month));
+        Assertions.assertEquals(Season.SPRING, SeasonUtils.printSeason(month));
     }
 
     @ParameterizedTest
     @EnumSource(value = Month.class, names = {"JUNE", "JULY", "AUGUST"})
     void shouldIndicateSummer(Month month) {
-        Assertions.assertEquals(Season.SUMMER.toString(), SeasonUtils.printSeason(month));
+        Assertions.assertEquals(Season.SUMMER, SeasonUtils.printSeason(month));
     }
 
     @ParameterizedTest
     @EnumSource(value = Month.class, names = {"SEPTEMBER", "OCTOBER", "NOVEMBER"})
     void shouldIndicateAutumn(Month month) {
-        Assertions.assertEquals(Season.AUTUMN.toString(), SeasonUtils.printSeason(month));
+        Assertions.assertEquals(Season.AUTUMN, SeasonUtils.printSeason(month));
     }
 }
